842715da559b96a0f4176b0c4fd3d712771a23e9
giullianoantonini <giullianoantonini@gmail.com>
2024-03-15 17:15:01 -0300

    1ª Tentativa

---
+++ script.js
@@ -0,0 +1,14 @@
+function executarCadastro(){
+    console.log ("Olá")
+}
+
+function executarListar(){
+    console.log ("Olá")
+
+}
+
+function executarCadastrar(){
+    console.log ("Olá")
+
+}
+
