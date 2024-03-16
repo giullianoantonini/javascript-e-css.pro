a8bb6f187f9a3c67392e5cad889b44df9f3754a4
Giulliano Antonini <giullianoantonini@gmail.com>
2024-03-15 21:01:28 -0300

    Atualizações

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-function executarCadastro(){
-    console.log ("Olá")
+function executarCadastro() {
+    console.log("Olá")
 }
 
-function executarListar(){
-    console.log ("Olá")
+function executarListar() {
+    console.log("Olá")
 
 }
 
-function executarCadastrar(){
-    console.log ("Olá")
+function executarCadastrar() {
+
+    const inputTitulo = document.getElementById("title")
+    const valorTitulo = inputTitulo.value
+
+    const inputDescricao = document.getElementById("description")
+    const valorDescricao = inputDescricao.value
+
+    const divSucesso = document.getElementById("divSucesso")
+
+    const divErro = document.getElementById("divErro")
+
+    if (valorTitulo === "" || valorDescricao === "") {
+        divErro.style.display = "block"
+    }
+    else {
+        divSucesso.style.display = "block"
+    }
+
+    console.log("Título:", valorTitulo);
+    console.log("Descrição:", valorDescricao);
 
 }
 
