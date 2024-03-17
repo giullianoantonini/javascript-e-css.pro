0a81fdb63a1d86293979137f8d79c0d76720323b
Giulliano Antonini <giullianoantonini@gmail.com>
2024-03-17 13:55:46 -0300

    Alterações

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function executarListar() {
 
 }
 
-function executarCadastrar() {
+async function executarCadastrar() {
 
     const inputTitulo = document.getElementById("title")
     const valorTitulo = inputTitulo.value
@@ -19,15 +19,51 @@ function executarCadastrar() {
 
     const divErro = document.getElementById("divErro")
 
+    const url = "https://api-aula.up.railway.app/livros"
+    const body = {
+        title: valorTitulo,
+        description: valorDescricao
+    }
+
+    const retorno = await postEmAPI(url, body)
+    console.log(retorno)
+
     if (valorTitulo === "" || valorDescricao === "") {
         divErro.style.display = "block"
+        divSucesso.style.display = "none"
+        alert(retorno)
     }
     else {
+        divErro.style.display = "none"
         divSucesso.style.display = "block"
+        alert(retorno)
     }
-
-    console.log("Título:", valorTitulo);
-    console.log("Descrição:", valorDescricao);
-
 }
 
+async function postEmAPI(url, body) {
+    const fetchResposta = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" }
+    });
+    const respostaEmJSON = await fetchResposta.json();
+    return respostaEmJSON;
+}
+
+async function mostrarLivros() {
+
+    const url = "https://api-aula.up.railway.app/livros"
+
+    const resposta = await fetch(url)
+    const livros = await resposta.json()
+
+    console.log(livros)
+
+    const divMostrarLivros = document.getElementById("divMostrarLivros")
+
+    divMostrarLivros.style.display = "block"
+
+    for (const livro of livros) {
+        divMostrarLivros.innerHTML += "Título: " + livro.title + ", Descrição: " + livro.description + "<br>";
+    }
+}
